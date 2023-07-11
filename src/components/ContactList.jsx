d5a0b7590108774705cca1238e1ec23872523ae3
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-11 12:02:31 +0330

    adding deleting loading and handle delete request on server

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 const CONTACTS_LIST_API = "http://localhost:3000/contacts";
+
 export const ContactList = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
   const [contactList, setContactList] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -16,30 +19,50 @@ export const ContactList = () => {
         console.log("Error fetching data:", e);
       }
     };
+
     fetchData();
   }, []);
+
+  const contactDeleteHandler = async (id) => {
+    try {
+      setIsDeleting(true);
+      await axios.delete(`${CONTACTS_LIST_API}/${id}`);
+      setIsDeleting(false);
+      const response = await axios.get(CONTACTS_LIST_API);
+      setContactList(response.data);
+    } catch (e) {
+      setIsDeleting(false);
+      console.log("Error:", e);
+    }
+  };
 
   return (
     <div>
       {isLoading ? <div>Loading...</div> : null}
-      {contactList.map(({ id, name, number }) => {
-        return (
-          <div className="contact-list">
-            <div key={id}>
-              <h3>{name}</h3>
-              <p>{number}</p>
-            </div>
-            <div className="change-btns">
+      {contactList.length === 0 && !isLoading ? (
+        <div>There is no Contacts in the list!</div>
+      ) : (
+        contactList.map(({ id, name, number }) => {
+          return (
+            <div key={id} className="contact-list">
               <div>
-                <button>Edit</button>
+                <h3>{name}</h3>
+                <p>{number}</p>
               </div>
-              <div>
-                <button>Delete</button>
+              <div className="change-btns">
+                <div>
+                  <button>Edit</button>
+                </div>
+                <div>
+                  <button onClick={() => contactDeleteHandler(id)}>
+                    {isDeleting ? <div>Deleting...</div> : <div>Delete</div>}
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 };
